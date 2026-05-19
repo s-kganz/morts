12ddefd42ec6a53ffaaa4ea19f47d8dee5f56c7e
s-kganz <ganzkeenan1@gmail.com>
2026-05-19 12:58:05 -0700

    use geojson for detections/tiles

--- scripts/process_ecostress_mgrs_tileset.py
+++ scripts/process_ecostress_mgrs_tileset.py
@@ -17,19 +17,19 @@ from typing import Any
 from pyproj import Transformer
 import lazycogs
 import xarray as xr
+import geopandas as gpd
 
 # Set up logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(message)s",
+    format="[%(asctime)s] %(message)s",
     datefmt="%H:%M:%S",
 )
 logger = logging.getLogger(__name__)
 
 # Check if we are on JupyterHub
 import os
-#is_jupyter_hub = "jupyter" in os.environ.get("HOSTNAME", default="")
-is_jupyter_hub = True
+is_jupyter_hub = "jupyter" in os.environ.get("HOSTNAME", default="")
 
 # LPCLOUD constants
 LPCLOUD_AWS_ENDPOINT="https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials"
@@ -47,14 +47,20 @@ ECO_ASSETS = [
 ]
 
 # Search constants
-SEARCH_TEMPORAL_RANGE="2024-06/2024-07"
+SEARCH_TEMPORAL_RANGE="2024-01/2025-01"
+# SEARCH_TEMPORAL_RANGE=None
 
 # Filter constants
-AFTERNOON_HOURS=[12, 13, 14, 15, 16]
+AFTERNOON_HOURS=list(range(12, 19))
 
 # Output constants
+# These match the grid of LCMS detections
 OUTPUT_CRS="EPSG:5071"
-OUTPUT_RES=70 # m
+OUTPUT_RES=300 # m
+
+# Datasets with objects
+DETECTIONS = gpd.read_parquet("data_working/detections.parquet")
+TILES = gpd.read_file("data_working/sentinel2_tiles_world_with_land.geojson")
 
 def _get_lpcloud_s3_obstore() -> S3Store:
     creds = requests.get(LPCLOUD_AWS_ENDPOINT).json()
@@ -119,18 +125,15 @@ def search_lpcloud_stac(collections: list[str], **kwargs) -> list[dict[str, Any]
     ).item_collection_as_dict()
     return items["features"]
     
-def sentinel_tile_to_shapely_geom(tile: str, buffer: float=-10_000) -> shapely.Geometry:
+def sentinel_tile_to_shapely_geom(tile: str) -> shapely.Geometry:
     return shapely.geometry.box(
-        *sentinel_tiles.bbox(tile).buffer(buffer).transform("EPSG:4326")
+        *sentinel_tiles.bbox(tile).transform("EPSG:4326")
     )
 
 def get_tileset_data_array(tileset: str) -> xr.DataArray:
     # Parse tiles
     tiles = tileset.split("-")
-    logger.info(f"Received tileset: {tiles}")
-    
-    # Get detections/nondetections in this tileset
-    
+            
     # Convert to geometries
     tile_bounds = list(map(sentinel_tile_to_shapely_geom, tiles))
     
@@ -139,7 +142,7 @@ def get_tileset_data_array(tileset: str) -> xr.DataArray:
     search_tasks = [
         search_lpcloud_stac(
             ECO_COLLECTIONS, 
-            bbox=tuple(shapely.bounds(tile)),
+            intersects=shapely.to_geojson(shapely.centroid(tile)),
             datetime=SEARCH_TEMPORAL_RANGE
         )
         for tile in tile_bounds
@@ -149,15 +152,41 @@ def get_tileset_data_array(tileset: str) -> xr.DataArray:
     if len(search_results) == 0:
         logger.error("Found no granules in search, exiting.")
         sys.exit(1)
-
-    # Filter air temperature granules for afternoon observations
-    search_results_filter = list(filter(
-        lambda x: _get_granule_local_hour(x) in AFTERNOON_HOURS,
-        search_results
-    ))
-    logger.info(f"After afternoon filter, {len(search_results_filter)} granules left")
     
-    # Sanitize assets
+    granules_by_collection = {
+        collection: list(filter(
+            lambda x: x["collection"] == collection,
+            search_results
+        ))
+        for collection in ECO_COLLECTIONS
+    }
+    
+    # MET/LST granules need to be in the afternoon and come from
+    # the same sensing time.    
+    granules_by_collection["ECO_L3T_MET_002"] = list(filter(
+        lambda x: _get_granule_local_hour(x) in AFTERNOON_HOURS,
+        granules_by_collection["ECO_L3T_MET_002"]
+    ))
+    
+    # Only keep LST granules that share a datetime with an afternoon MET granule
+    met_datetimes = {
+        f["id"].split("_")[-3]
+        for f in granules_by_collection["ECO_L3T_MET_002"]
+    }
+    granules_by_collection["ECO_L2T_LSTE_002"] = list(filter(
+        lambda x: x["id"].split("_")[-3] in met_datetimes,
+        granules_by_collection["ECO_L2T_LSTE_002"]
+    ))
+    
+    # Collect all granules back together
+    search_results_filter = list(reduce(list.__add__, granules_by_collection.values()))
+    logger.info("After filtering, granule distribution is")
+    logger.info("; ".join([f"{c}: {len(granules_by_collection[c])}" for c in ECO_COLLECTIONS]))
+    
+    if len(granules_by_collection["ECO_L2T_LSTE_002"]) != len(granules_by_collection["ECO_L3T_MET_002"]):
+        logger.warning("LST and MET have differing numbers of granules!")
+    
+    # Sanitize asset keys for loading
     search_results_sanitized = list(map(
         _sanitize_item_asset_keys,
         search_results_filter
@@ -172,15 +201,15 @@ def get_tileset_data_array(tileset: str) -> xr.DataArray:
     if is_jupyter_hub:
         s3_store = _get_lpcloud_s3_obstore()
     else:
-        s3_store = None
+        logging.error("Cannot access granules over HTTP, exiting.")
+        sys.exit(0)
     transformer = Transformer.from_crs("EPSG:4326", OUTPUT_CRS, always_xy=True)
     
     total_bbox = shapely.transform(
         shapely.union_all(tile_bounds),
-        transformation=transformer.transform,
+        transformation=transformer.transform_bounds,
         interleaved=False
     )
-    total_bbox = shapely.buffer(total_bbox, 10_000)
     
     tile_da = lazycogs.open(
         parquet_path,
@@ -194,9 +223,30 @@ def get_tileset_data_array(tileset: str) -> xr.DataArray:
     
     return tile_da
     
+def get_objects_in_tiles(tile_ids: str, tile_geoms: gpd.GeoDataFrame, objects: gpd.GeoDataFrame):
+    '''
+    Returns a subset of `objects` that are within the MGRS grids in `tileset`.
+    '''
+    tileset = tile_ids.split("-")
+    tile_geom_subset = tile_geoms[tile_geoms["Name"].isin(tileset)]
+    object_subset = objects[objects["geometry"].intersects(tile_geom_subset.to_crs(objects.crs).union_all(), align=False)]
+    return object_subset
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--tileset", type=str, required=True)
     args = parser.parse_args()
+    logger.info(f"Received tileset: {args.tileset}")
+
+    objects = get_objects_in_tiles(
+        args.tileset,
+        TILES,
+        DETECTIONS
+    )
+    
+    n_detections = (objects["tmin"] != -1).sum()
+    n_nondetections = (objects["tmin"] == -1).sum()
+    logger.info(f"Found {n_detections} detections and {n_nondetections} nondetections.")
+    
     da = get_tileset_data_array(args.tileset)
